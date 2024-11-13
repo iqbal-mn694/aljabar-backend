0888ee68d08a5cc7b5d018fa213e28b299d8a4f6
iqbal-mn694 <garenahiber134@gmail.com>
2024-11-13 22:06:36 +0700

    cors issue

--- tubesAljabar/settings.py
+++ tubesAljabar/settings.py
@@ -27,10 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Ubah sesuai dengan port frontend Anda
+    "http://localhost:8000",
+]
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
